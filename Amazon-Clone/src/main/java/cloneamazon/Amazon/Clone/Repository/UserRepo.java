a3540ed2b2e14a479a6247baab9df9947451bfaf
Sathwik <114406254+sathwik-y@users.noreply.github.com>
2024-12-21 17:50:39 +0530

    Delete Amazon-Clone directory

--- Amazon-Clone/src/main/java/cloneamazon/Amazon/Clone/Repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package cloneamazon.Amazon.Clone.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import cloneamazon.Amazon.Clone.Entity.Users;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<Users,Long> {
-    public Users getByEmail(String email);
-}
